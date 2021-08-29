205dd87bc697e3871f90690bef8567124e7d530d
Eric Meinzer <emeinzer7@gmail.com>
2021-08-29 13:09:44 -0500

    how to button?

--- calc.js
+++ calc.js
@@ -1,1 +1,42 @@
 console.log('connected');
+
+// let outputScreen = document.getElementById("output-screen");
+
+// function calculateTotal(updateDisplay) {
+//   var elem = document.getElementById('display');
+//   elem.style.color = newColor;
+// }
+
+// <p id="para">Some text here</p>
+// <button onclick="changeColor('blue');">blue</button>
+// <button onclick="changeColor('red');">red</button>
+
+// function changeColor(newColor) {
+//   var elem = document.getElementById('para');
+//   elem.style.color = newColor;
+
+// --- new part here --
+
+// function display(num) {
+//   outputScreen.value += num;
+// }
+// function calculate() {
+//   try {
+//     outputScreen.value = eval(outputScreen.value);
+//   }
+//   catch(err) {
+//     alert('Invalid')
+//   }
+// }
+//
+// function clear() {
+//   outputScreen.vale = '';
+// }
+// function del() {
+//   outputScreen.value = outputScreen.value.slice(0, -1);
+// }
+//
+//
+// function calcFunction() {
+//   document.getElementById("demo").innerHTML = 'yes';
+// }
